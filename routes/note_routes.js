7deab735c7038f2a2168fb1158c2a5ed96629ebb
Vamsi Ravi <ravivamsi@yahoo.com>
2018-06-04 16:10:48 -0500

    Translator Node Application Initialized

---
+++ routes/note_routes.js
@@ -0,0 +1,17 @@
+module.exports = function(app, db) {
+  app.post('/translate/:string/from/:source/to/:target', (req, res) => {
+
+    var translated = "translated text goes here";
+    const translate = require( 'google-translate-api');
+
+    translate(req.params.string, {to: 'en'}).then(result => {
+    console.log(result.text);
+    translated = result.text;
+}).catch(err => {
+    console.error(err);
+});
+    res.send(translated);
+   //res.send('Translator will convert '+req.params.string+' from ' + req.params.source + ' to ' + req.params.target);
+  });
+};
+ 
