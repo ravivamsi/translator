3f6ce54a80edc41cc1fa3a61517a1c1fef8e5c05
Vamsi Ravi <ravivamsi@yahoo.com>
2018-06-04 19:03:08 -0500

    Translation works the second time

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,16 +1,20 @@
 module.exports = function(app, db) {
+
+
+  var translated = "translated text goes here";
+
   app.post('/translate/:string/from/:source/to/:target', (req, res) => {
 
-    var translated = "translated text goes here";
     const translate = require( 'google-translate-api');
 
-    translate(req.params.string, {to: 'en'}).then(result => {
+    translate(req.params.string, {to: 'en'}).then(result =>{
     console.log(result.text);
     translated = result.text;
-    console.log(translated);
-}).catch(err => {
+}
+).catch(err => {
     console.error(err);
 })
+    console.log('Translated to:'+ translated)
     res.send(translated);
    //res.send('Translator will convert '+req.params.string+' from ' + req.params.source + ' to ' + req.params.target);
   });
